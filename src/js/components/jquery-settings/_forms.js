5e21d118b57f5e935ee975c903040965f71b22bd
MediaBay <med.bay@yandex.ru>
2021-05-31 15:02:31 +0300

    Исправлена ошибка с валидатором форм

--- src/js/components/jquery-settings/_forms.js
+++ src/js/components/jquery-settings/_forms.js
@@ -1,15 +1,16 @@
-;(function() {
+;
+(function() {
   let thanksPopupTimer,
     thanksPopup = $('.thanks-popup'),
     overlay = $('.overlay');
 
-    closeThanksPopup = function() {
-      thanksPopup.css('animation', 'fadeOut .5s');
+  closeThanksPopup = function() {
+    thanksPopup.css('animation', 'fadeOut .5s');
 
-      if (overlay.hasClass('active')) {
-        overlay.css('animation', 'fadeOut .5s');
-      }
-    };
+    if (overlay.hasClass('active')) {
+      overlay.css('animation', 'fadeOut .5s');
+    }
+  };
 
   $('form').each(function() {
     $(this).validate({
@@ -58,9 +59,9 @@
         for (let i = 0; i < inputs.length; i++) {
           inputs[i].classList.remove('filled');
         }
-        
+
         $(this)[0].resetForm();
-        
+
         thanksPopup.addClass('active');
         if (!overlay.hasClass('active')) {
           overlay.addClass('active');
@@ -69,42 +70,43 @@
           closeThanksPopup();
         }, 3000);
       }
-      });
     });
+  });
 
-    thanksPopup.on('click', function() {
-      closeThanksPopup();
-      clearTimeout(thanksPopupTimer);
-    });
+  thanksPopup.on('click', function() {
+    closeThanksPopup();
+    clearTimeout(thanksPopupTimer);
+  });
 
-    $('.thanks-popup, .overlay').on('animationend', function() {
-      if (event.animationName === 'fadeOut') {
-        $(this).removeClass('active');
-        $(this).css('animation', '');
-      }
-    });
-
-    // form beforesubmit validate
-    $('form .btn').on('click', function() {
-      if (!$(event.target).parents('form').valid()) {
-        event.preventDefault();
-      }
-    });
-
-  })();
-
-
-  $('.field__inp').on('input', function() {
-    if ($(this).val() !== '') {
-      $(this).addClass('filled');
-    } else {
-      $(this).removeClass('filled');
+  $('.thanks-popup, .overlay').on('animationend', function() {
+    if (event.animationName === 'fadeOut') {
+      $(this).removeClass('active');
+      $(this).css('animation', '');
     }
   });
 
-  $('.product-name-inp').val($('.product-hero-sect__title').text());
+  // form beforesubmit validate
+  $('form .btn').on('click', function() {
+    if (!$(event.target).parents('form').valid()) {
+      event.preventDefault();
+    }
+  });
+
+})();
 
 
+$('.field__inp').on('input', function() {
+  if ($(this).val() !== '') {
+    $(this).addClass('filled');
+  } else {
+    $(this).removeClass('filled');
+  }
+});
+
+$('.product-name-inp').val($('.product-hero-sect__title').text());
+
+
+if ($.validator) {
   $.validator.methods.userPhone = function(value, element) {
     return /\+7\([0-9]{3}\)[0-9]{3}\-[0-9]{2}\-[0-9]{2}/.test(value);
   };
@@ -112,3 +114,4 @@
   $.validator.methods.userName = function(value, element) {
     return /^[а-яёА-ЯЁa-zA-Z\s]+$/.test(value);
   };
+}
